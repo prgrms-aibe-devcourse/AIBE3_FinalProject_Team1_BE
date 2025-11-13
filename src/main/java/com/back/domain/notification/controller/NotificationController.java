52a771e278c53ac5205de192051a13f79e4b32e8
ehgk4245 <ehgkrk14@naver.com>
2025-11-13 15:41:16 +0900

    feat: 계층 구조 세팅

---
+++ src/main/java/com/back/domain/notification/controller/NotificationController.java
@@ -0,0 +1,14 @@
+package com.back.domain.notification.controller;
+
+import com.back.domain.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+}
---
+++ src/main/java/com/back/domain/notification/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.back.domain.notification.service;
+
+import com.back.domain.notification.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+}
