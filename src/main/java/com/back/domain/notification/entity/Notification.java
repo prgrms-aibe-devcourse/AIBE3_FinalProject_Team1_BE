0c7dbe79135b50e7517819558009586456847682
ehgk4245 <ehgkrk14@naver.com>
2025-11-13 12:46:23 +0900

    feat: 알림 Entity 작성

---
+++ src/main/java/com/back/domain/notification/entity/Notification.java
@@ -0,0 +1,26 @@
+package com.back.domain.notification.entity;
+
+import com.back.domain.member.entity.Member;
+import com.back.domain.notification.common.NotificationType;
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+
+    private Long targetId;
+
+    private Boolean isRead;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
