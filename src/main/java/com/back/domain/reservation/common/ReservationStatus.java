ccb3c43f606d9974e61240325b8d89f7a653277e
1J-Choi <chldnjswp110@naver.com>
2025-11-14 13:57:37 +0900

    [REFACTOR] ReservationStatusSubject ENUM 추가

--- src/main/java/com/back/domain/reservation/common/ReservationStatus.java
+++ src/main/java/com/back/domain/reservation/common/ReservationStatus.java
@@ -4,29 +4,31 @@ import lombok.Getter;
 
 @Getter
 public enum ReservationStatus {
-    PENDING_APPROVAL("승인 대기", false),
-    PENDING_PAYMENT("결제 대기", false),
-    PENDING_PICKUP("수령 대기", false),
-    SHIPPING("배송 중", false),
-    INSPECTING_RENTAL("대여 검수", false),
-    RENTING("대여 중", false),
-    PENDING_RETURN("반납 대기", false),
-    RETURNING("반납 중", false),
-    RETURN_COMPLETED("반납 완료", true),
-    INSPECTING_RETURN("반납 검수", true),
-    PENDING_REFUND("환급 예정", true),
-    REFUND_COMPLETED("환급 완료", true),
-    LOST_OR_UNRETURNED("미반납/분실", false),
-    CLAIMING("청구 진행", false),
-    CLAIM_COMPLETED("청구 완료", true),
-    REJECTED("승인 거절", false),
-    CANCELLED("예약 취소", false);
+    PENDING_APPROVAL("승인 대기", false, ReservationStatusSubject.SYSTEM_OR_ANY),
+    PENDING_PAYMENT("결제 대기", false, ReservationStatusSubject.HOST),
+    PENDING_PICKUP("수령 대기", false, ReservationStatusSubject.SYSTEM_OR_ANY),
+    SHIPPING("배송 중", false, ReservationStatusSubject.HOST),
+    INSPECTING_RENTAL("대여 검수", false, ReservationStatusSubject.SYSTEM_OR_ANY),
+    RENTING("대여 중", false, ReservationStatusSubject.GUEST),
+    PENDING_RETURN("반납 대기", false, ReservationStatusSubject.SYSTEM_OR_ANY),
+    RETURNING("반납 중", false, ReservationStatusSubject.GUEST),
+    RETURN_COMPLETED("반납 완료", true, ReservationStatusSubject.SYSTEM_OR_ANY),
+    INSPECTING_RETURN("반납 검수", true, ReservationStatusSubject.HOST),
+    PENDING_REFUND("환급 예정", true, ReservationStatusSubject.HOST),
+    REFUND_COMPLETED("환급 완료", true, ReservationStatusSubject.SYSTEM_OR_ANY),
+    LOST_OR_UNRETURNED("미반납/분실", false, ReservationStatusSubject.HOST),
+    CLAIMING("청구 진행", false, ReservationStatusSubject.HOST),
+    CLAIM_COMPLETED("청구 완료", true, ReservationStatusSubject.SYSTEM_OR_ANY),
+    REJECTED("승인 거절", false, ReservationStatusSubject.HOST),
+    CANCELLED("예약 취소", false, ReservationStatusSubject.GUEST);
 
     private final String description;
     private final boolean isReviewable;
+    private final ReservationStatusSubject statusSubject;
 
-    ReservationStatus(String description, boolean isReviewable) {
+    ReservationStatus(String description, boolean isReviewable, ReservationStatusSubject statusSubject) {
         this.description = description;
         this.isReviewable = isReviewable;
+        this.statusSubject = statusSubject;
     }
 }
---
+++ src/main/java/com/back/domain/reservation/common/ReservationStatusSubject.java
@@ -0,0 +1,8 @@
+package com.back.domain.reservation.common;
+
+public enum
+ReservationStatusSubject {
+    HOST,
+    GUEST,
+    SYSTEM_OR_ANY
+}
